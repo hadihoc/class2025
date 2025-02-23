88f59eba9e9e719e228fca828915a1d1f8764d22
scf2025 <hanna.yeu28@yahoo.com>
2025-02-22 22:52:55 -0500

    updated first quiz
    Added comments to the script.js

--- src/week3/scripts.js
+++ src/week3/scripts.js
@@ -35,44 +35,49 @@ let answerKeys=[
 let scores = 0;
 let currentQuestionIndex = 0;
 
+/*This function will display the question and its multiple choices answer */
 function setupQuestion(){
-    try{
 
-        questionElement.innerHTML = questions[currentQuestionIndex]; 
-        answerElement.innerHTML = "";
-        possibleAnswers[currentQuestionIndex].forEach(element =>{
-            let thisAnswer = document.createElement("li");
-            thisAnswer.innerHTML = element;  
-            thisAnswer.onclick =(event) =>{
-                if(event.target.innerHTML == answerKeys[currentQuestionIndex]){
+    if(currentQuestionIndex < questions.length){  /*If the current index of the question array is less than the length of the questions array*/
+        questionElement.innerHTML = questions[currentQuestionIndex]; /*Display a multiple-choice question in sequence order, beginning with index 0 */
+        answerElement.innerHTML = "";  /*Clear the place holders of a set of possible answers */
+        possibleAnswers[currentQuestionIndex].forEach(element =>{  /*Iterate through the possible answers array  */
+            let thisAnswer = document.createElement("li");  /*Create a list item for each possible answer */
+            thisAnswer.innerHTML = element;  /*Displays a possible answer in sequence order */
+            answerElement.appendChild(thisAnswer); /*Add the possible answer to the parent element */
+            thisAnswer.onclick =(event) =>{  /*If user clicks on this possible answer, excutes the arrow function */
+                if(event.target.innerHTML == answerKeys[currentQuestionIndex]){  /*If the selected possible answer is matched with the correct answer in answerKeys array  */
                    /* alert("Correct!");*/
-                    currentQuestionIndex++;
-                    scores++;
-                    setupQuestion();
+                    currentQuestionIndex++;  /*Increament currentQuestionIndex to 1 */
+                    scores++;  /*Add one to the score keeper or variable*/
+                    setupQuestion();  /*Call setupQueston function to display the next question */
                 }
-                else{
+                else{  /*If the selected answer is incorrect*/ 
                     /*alert("Wrong answer! It's " + answerKeys[currentQuestionIndex])*/
-                    currentQuestionIndex++;
-                    setupQuestion();
+                    currentQuestionIndex++; /*Increament the currentFQuestionIndex by one */
+                    setupQuestion();  /*Calls setupQuestion function to display the next question */
                 }
             };
-            answerElement.appendChild(thisAnswer);
         });
-    }catch (error){
-        questionsDiv.style.visibility = "hidden";
-        quizGameHeader.style.visibility = "hidden";
-        gameOver.style.visibility = "visible";
-        finalScore.innerHTML = scores + " out of " + questions.length;
     }
+    else{  /*If the current index of the question array is equal to the length of the quetions array*/
+        questionsDiv.style.visibility = "hidden";  /*Hide the question screen*/
+        quizGameHeader.style.visibility = "hidden"; 
+        gameOver.style.visibility = "visible"; /*Set the game over screen visible*/
+        finalScore.innerHTML = scores + " out of " + questions.length; /*Display the final score */
+    }
+   
 }
 
+/*When this function is called, the start screen will be invisible and it will be replace by the questions screen*/
 function startQuiz(){
     startScreen.style.visibility = 'hidden';
     questionsDiv.style.visibility = 'visible';
     quizGameHeader.style.visibility = 'visible';
-    setupQuestion();
+    setupQuestion();  /* Calls setup question function*/
 }
 
+/* When this function is called, it will refresh the page */
 function restartQuiz(setupQuestion){
     location.reload();
 }
