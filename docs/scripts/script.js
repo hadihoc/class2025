8a2db6e2abb3cb02b7de925db4c54fc0b55184cf
scf2025 <hanna.yeu28@yahoo.com>
2025-03-08 00:37:03 -0500

    created contacts form

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -14,34 +14,83 @@ let newElement = document.createElement('h4');
 // Initialize a dictionary contains other info about Anna
 let aboutAnna = {
    'Other Degree': 'Electronics Engineering Technology',
-   'Interests': 'Learning math & programming languages',
-   'Hobbies': 'Practicing classical piano, guitar, and singing',
+   'Interests': 'Learning mathematics & coding',
+   'Hobbies': 'Practicing piano, guitar, and singing',
    'Traveled Places': 'France, Italy, and England',
  };
 
+ let images = {'Traveled Places': "'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcRIPCIx39wdVSrLEi4kgP2-B0QgzPmVovT17B6cleo02iF0oRNPa-lAp4KrvF_U6ZUDep6p6W2LQbX3N-455Fi0Z4-FPevyf8Ek3vNpFA'",
+               'Other Degree':'https://t3.ftcdn.net/jpg/09/74/89/20/240_F_974892088_19RtAYgktnA2lx8H7tWZAXgblAikWXkm.jpg',
+               'Interests':'https://m.media-amazon.com/images/I/51bxnNLuheL.jpg',
+               'Hobbies':'https://thumbs.dreamstime.com/b/music-notes-background-7533715.jpg?w=768',
+            
+ };
  // Set 3 innerDivs to invisible mode
 function setInnerDivsInvisible(){
    getProfessionDiv.style.display = 'none';
    getCertificationDiv.style.display ='none';
    getEducationDiv.style.display = 'none';
 }
-// This function sets other option div to visible  
-function setOtherOptionsDivVisible(){
+// This function sets other option div to visible 
+function setOtherOptionsDivVisible(option){
+   const value = option;
       getOtherOptionDiv.style.display = 'visible';
       getOtherOptionDiv.style.width = '50%';
       getOtherOptionDiv.style.height = '60%';
       getOtherOptionDiv.style.position = 'relative';
-      getOtherOptionDiv.style.backgroundColor = 'rgb(248, 232, 221)';
       getOtherOptionDiv.style.marginLeft = '20px';
-      getOtherOptionDiv.style.color = 'purple';
+      getOtherOptionDiv.style.color = 'whitesmoke';
       getOtherOptionDiv.style.paddingLeft = '50px';
-      getOtherOptionDiv.style.paddingTop = '50px';
+      getOtherOptionDiv.style.paddingTop = '50px'; 
+      getOtherOptionDiv.style.position = 'relative';
+      getOtherOptionDiv.style.backgroundRepeat = 'no-repeat';
 
-}
+      if (value === 'Traveled Places'){
+         getOtherOptionDiv.style.backgroundImage = "url('https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcRIPCIx39wdVSrLEi4kgP2-B0QgzPmVovT17B6cleo02iF0oRNPa-lAp4KrvF_U6ZUDep6p6W2LQbX3N-455Fi0Z4-FPevyf8Ek3vNpFA')";
+      }
+      else if(value === 'Other Degree'){
+         getOtherOptionDiv.style.backgroundImage = "url('https://t3.ftcdn.net/jpg/09/74/89/20/240_F_974892088_19RtAYgktnA2lx8H7tWZAXgblAikWXkm.jpg')";
+      }
+      else if (value === 'Interests'){
+         getOtherOptionDiv.style.backgroundImage = "url('../webLanguages.png')";
+      }
+      else if (value === 'Hobbies'){
+         getOtherOptionDiv.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2017/03/24/15/26/piano-2171359_1280.jpg')";
+   
+      }
+}  
+
 // This function creates a <h4> tags and displays other degree
-function createH4Elements(option){  
-   newElement.innerHTML = aboutAnna[option];
-   getOtherOptionDiv.appendChild(newElement);   
+function createH4Elements(option){
+   const value = option;    
+   newElement.innerHTML = aboutAnna[value];
+   getOtherOptionDiv.appendChild(newElement);
+   newElement.style.textAlign = 'center'; 
+   newElement.style.backgroundColor = 'black';
+   newElement.style.color = 'white';
+
+   if (value === 'Traveled Places'){
+      newElement.style.width = '200px';
+      newElement.style.marginLeft = '50px';
+      newElement.style.backgroundColor = '#87CEEB';
+      newElement.style.color = 'black';
+   }
+   else if(value === 'Other Degree'){
+      newElement.style.width = '260px';
+   }
+   else if (value === 'Interests'){
+      newElement.style.width = '300px';
+      newElement.style.color = 'yellow';
+      newElement.style.backgroundColor = 'transparent';
+      getOtherOptionDiv.style.paddingTop = '0%';
+   }
+   else if (value === 'Hobbies'){
+      newElement.style.width = '260px';
+      newElement.style.backgroundColor = 'transparent';
+      newElement.style.color = 'orange';
+      getOtherOptionDiv.style.paddingTop = '0%';
+      getOtherOptionDiv.style.paddingLeft = '0%';
+   }
 }
 
 
@@ -52,22 +101,22 @@ getSelectedElement.addEventListener('change', (Event) =>{
    switch (option){
       case 'Other Degree':    
          setInnerDivsInvisible();
-         setOtherOptionsDivVisible();
+         setOtherOptionsDivVisible(option);
          createH4Elements(option);
         break;
       case 'Interests':
          setInnerDivsInvisible();
-         setOtherOptionsDivVisible();
+         setOtherOptionsDivVisible(option);
          createH4Elements(option);
          break;
       case 'Hobbies':
          setInnerDivsInvisible();
-         setOtherOptionsDivVisible();
+         setOtherOptionsDivVisible(option);
          createH4Elements(option);
          break;
       case 'Traveled Places':
          setInnerDivsInvisible();
-         setOtherOptionsDivVisible();
+         setOtherOptionsDivVisible(option);
          createH4Elements(option);
          break;
       case 'select one':
