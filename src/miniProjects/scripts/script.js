1c171cc4cefa53161a1418ddc50590b88de51045
scf2025 <hanna.yeu28@yahoo.com>
2025-03-12 14:02:06 -0400

    created mini project folder

---
+++ src/miniProjects/scripts/script.js
@@ -0,0 +1,10 @@
+function open_sideBar(){
+    document.getElementById('sideBar').style.display = 'block';
+    document.getElementById('content').style.width = '400px';
+}
+
+function close_sideBar(){
+    document.getElementById('sideBar').style.display = 'none';
+    document.getElementById('content').style.width = '600px';
+   
+}
