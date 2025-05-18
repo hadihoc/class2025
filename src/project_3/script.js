d99a7d7d390ccfd52a4c17fe73c607efba9d8d84
scf2025 <hanna.yeu28@yahoo.com>
2025-05-18 17:17:29 -0400

    project_3
    added administration screen

--- src/project_3/script.js
+++ src/project_3/script.js
@@ -1,3 +1,96 @@
+const admin = document.getElementById("admin_login");
+const checkoutBtn = document.getElementById("ckout_btn");
+const checkinBtn = document.getElementById("ckin_btn");
+const viewBtn = document.getElementById("view_btn");
+checkoutBtn.disabled = true;
+checkinBtn.disabled = true;
+viewBtn.disabled = true;
+
+admin_dict = {
+              "anna@scf": "Iam12345",
+              "andrea@scf": "Sheis6789"               
+}
+
+/* Add event listener to the administration login button*/
+admin.addEventListener('click', ()=> {
+
+  /* Create a div tag for log in screen*/
+  const login_div = document.getElementById("login_screen");
+
+  /* Create an input tag for user name */
+  const user_name_input = document.createElement("input");
+  user_name_input.type = 'text';
+  user_name_input.placeholder = 'user name';
+  user_name_input.id = 'user';
+  user_name_input.style.height = '30px';
+  user_name_input.style.width = '150px';
+  
+  /* Create an input for password */
+  const password_input = document.createElement("input");
+  password_input.type = 'text';
+  password_input.placeholder = 'password';
+  password_input.id = 'password';
+  password_input.style.height = '30px';
+  password_input.style.width = '150px';
+  
+  /* Create  a button for submitting user name and password*/
+  const submitBtn = document.createElement("button"); 
+  submitBtn.type = 'button';
+  submitBtn.id = 'submit';
+  submitBtn.innerText = 'SUBMIT';
+  submitBtn.style.height = '30px';
+  submitBtn.style.width = '80px';
+  
+  
+  /* Create a button for log out Library System*/
+  const logoutBtn = document.createElement("button");
+  logoutBtn.type = 'button';
+  logoutBtn.id = 'submit';
+  logoutBtn.innerText = 'LOG OUT';
+  logoutBtn.style.height = '30px';
+  logoutBtn.style.width = '80px';
+  
+  /* Add two input tags and two button tags inside log in screen div*/
+  login_div.appendChild(user_name_input);
+  login_div.appendChild(password_input);
+  login_div.appendChild(submitBtn);
+  login_div.appendChild(logoutBtn);
+  
+  /* Get elements by their ids*/
+  const submit = document.getElementById("submit");
+  const user_name = document.getElementById("user");
+  const password = document.getElementById("password");
+  
+  /* Set the log in screen div background color*/
+  login_div.style.background = 'whitesmoke';
+  login_div.style.width = '540px';
+  login_div.style.paddingLeft = '10px';
+  /* Add event listener to a submit button, if click occurs, check for user name and password */
+  submit.addEventListener('click', () => {
+    if(user_name.value in admin_dict){
+      Object.values(admin_dict).forEach(() => {
+        /*Object.values(admin_dict) extracts all values from the admin_dict object and returns them as an array 
+          .includes(targetValue) checks if the array contains 
+          The ! negates the result — so if the password.value is not found, the alert() is triggered.*/
+        if (!Object.values(admin_dict).includes(password.value)){
+            alert("⚠️ You've entered an invalid password ⚠️");
+        }
+        else{
+          alert("true");
+            checkoutBtn.disabled = false;
+            checkinBtn.disabled = false;
+            viewBtn.disabled = false; 
+                  
+        }
+      })
+    }else{
+      alert("⚠️ You've entered an invalid user name ⚠️");
+    }
+  })
+
+admin.disabled = true;
+})
+
 /* Create a placeholder variable called db for:
    db gets assigned the actual database connection object (IDBDatabase)
    use db to start transactions, access object stores, read/write data*/
